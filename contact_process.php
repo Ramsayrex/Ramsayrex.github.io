1d2c867cc74ba6a398e1de287fa95e6a5f4d6ceb
Ramsayrex <mrrexatron@gmail.com>
2020-06-20 11:07:29 -0400

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,25 +1,20 @@
 <?php
 
-$to = 'bader.ramsay@gmail.com'; // replace this mail with yours
-    $firstname = $_POST["fname"];
-    $email= $_POST["email"];
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $to = "bader.ramsay@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $subject = $_REQUEST['subject'];
+    $number = $_REQUEST['number'];
+    $cmessage = $_REQUEST['message'];
 
-    $message ='<table style="width:100%">
-        <tbody><tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-    </tbody></table>';
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'The message has been sent.';
-    }else{
-    echo 'failed';
-}
+    $subject = "You have a message from your Bitmap Photography.";
+
     $logo = 'img/logo.png';
     $link = '#';
 
